f2c2dfddeb91292955b0421c394ceb5fe791a829
Luis Fernando de Oliveira <luisfernandophsc@gmail.com>
2026-04-12 12:21:01 -0300

    Add PPOAgent V1 and move DQN to networks

--- core/networks.py
+++ core/networks.py
@@ -37,3 +37,57 @@ class Critic(BaseNetwork):
 
     def forward(self, state, action):
         return self.net(torch.cat([state, action], 1))
+    
+
+class DQN(nn.Module):
+
+    def __init__(self, num_obs, num_actions):
+        super(DQN, self).__init__()
+
+        self.net = nn.Sequential(nn.Linear(num_obs, 64), 
+                                 nn.ReLU(),
+                                 nn.Linear(64, 64), 
+                                 nn.ReLU(),
+                                 nn.Linear(64, num_actions))
+        
+
+    def forward(self, x):
+        return self.net(x)
+    
+    @property
+    def output_size(self):
+        return self.net[-1].out_features
+    
+
+class DQNCNN(nn.Module):
+
+    def __init__(self, num_obs, num_actions):
+        super(DQNCNN, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(num_obs[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+        # Calcula o tamanho da saída da CNN dinamicamente para permitir diferentes tamanhos de ecrã
+        with torch.no_grad():
+            dummy_input = torch.zeros(1, *num_obs)
+            cnn_out_dim = self.features(dummy_input).flatten().shape[0]
+            
+        self.net = nn.Sequential(
+            nn.Linear(cnn_out_dim, 512), 
+            nn.ReLU(),
+            nn.Linear(512, num_actions)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        return self.net(x)
+    
+    @property
+    def output_size(self):
+        return self.net[-1].out_features
---
+++ scenarios/cart_pole/main_ppo.py
@@ -0,0 +1,75 @@
+
+import torch
+
+import gymnasium as gym
+from gymnasium.wrappers import RecordVideo
+from pathlib import Path
+
+from core.ppo_agent import PPOAgent
+
+from core.trainer import Trainer
+from core.logtrigger import segmented_limit_trigger
+from core.info_overlay import InfoOverlay
+
+
+# Diretório onde o script está localizado
+BASE_DIR = Path(__file__).resolve().parent
+# Cria diretórios para salvar modelos e vídeos, se não existirem
+(BASE_DIR / "model").mkdir(parents=True, exist_ok=True)
+(BASE_DIR / "VideosCartPole_PPO").mkdir(parents=True, exist_ok=True)
+
+ENV_NAME = 'CartPole-v1' 
+LR = 3e-4  # 0.0005  
+GAMMA = 0.99 # 0.95 # Fator de desconto
+EPS_CLIP = 0.2        # Parâmetro de clipping do PPO
+
+NUM_EPISODES = 1500
+
+K_EPOCHS = 10          # Quantas vezes atualizar a rede com o mesmo batch
+    
+UPDATE_TIMESTEPS = 400 # Atualizar a cada X passos
+
+
+
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "cpu"
+)
+
+temp_env = gym.make(ENV_NAME)
+obs, info = temp_env.reset()
+n_observations = len(obs)
+n_actions = temp_env.action_space.n
+temp_env.close()
+
+
+agent = PPOAgent(state_dim=n_observations, 
+                 action_dim=n_actions,
+                 device=device,
+                 lr=LR,
+                 gamma=GAMMA,
+                 eps_clip=EPS_CLIP,
+                 k_epochs=K_EPOCHS,
+                 update_timesteps=UPDATE_TIMESTEPS)
+
+
+format_type = "stories"  # 'stories'
+name_prefix = "Mountain-Car-dqn_train"
+
+# Setup environment with InfoOverlay and RecordVideo
+dqn_env = gym.make(ENV_NAME, render_mode="rgb_array")
+dqn_env = InfoOverlay(dqn_env, format_type = format_type)
+dqn_env = RecordVideo(dqn_env, video_folder=f"{BASE_DIR}/VideosCartPole_PPO", name_prefix="CartPole-PPO_train_v1", episode_trigger=segmented_limit_trigger)
+
+trainer = Trainer(env=dqn_env, agent=agent, warmup_steps=0)
+
+try:
+    
+    trainer.train(num_episodes=NUM_EPISODES)
+    agent.save(f"{BASE_DIR}/model/CartPole_PPO.pt")
+
+except KeyboardInterrupt:
+    print("\nTreinamento interrompido. Salvando modelo atual")
+    agent.save(f"{BASE_DIR}/model/CartPole_PPO_interrompido.pt")
+
+
